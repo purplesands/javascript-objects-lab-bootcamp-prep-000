94500389b78aa13aefc65bf2080d9765876d2e55
Joe Mullen <mullenjoe13@gmail.com>
2018-11-23 16:10:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,8 +3,6 @@ var recipes = {
 }
 
  function updateObjectWithKeyAndValue(object, key, value) {
-Object.assign({}, key, value)
- 
-  return object
+ return Object.assign({}, object, { [key]: value })
+
  }
- 
